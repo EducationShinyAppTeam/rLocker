1a8400aba0aa359ba1b09629f945b04d5d10db6a
ROBERT PATRICK CAREY III <rpc5102@psu.edu>
2019-05-16 15:30:05 -0400

    Converted R6 to S4

--- R/actor.R
+++ R/actor.R
@@ -1,34 +1,28 @@
 #'actor
+#'
 #'@import uuid
 NULL
 
-# Actor class definition
-Actor <- R6Class("Actor", 
-  public = list(
-    initialize = function(...) {
-      x <- list(...)
-      
-      # Set required defaults
-      private$objectType <- ifelse(is.null(x$type), "Agent", x$type)
-      private$name <- ifelse(is.null(x$name), UUIDgenerate(), x$name)
-      private$mbox <- ifelse(is.null(x$mbox), "mailto:test@example.org", x$mbox)
-    }
-  ),
-  private = list(
-    account = list(
-      name = NULL,
-      homePage = NULL
-    ),
-    objectType = NULL,
-    name = NULL,
-    mbox = NULL,
-    mbox_sha1sum = NULL,
-    openid = NULL,
-    group_members = NULL
-  )
+Actor <- setClass("Actor", 
+   slots = c(
+     account = list(
+       name = "character",
+       homePage = "character"
+     ),
+     group_members = "character",
+     mbox = "character",
+     name = "character", 
+     objectType = "character",
+     openid = "character"
+   ), 
+   prototype = list(
+     objectType = "Agent",
+     name = uuid::UUIDgenerate(),
+     mbox = "mailto:test@example.org"
+   )
 )
 
-Actor.getActorTypes = function(){
+Actor.getActorTypes = function() {
   types <- c(
     "Agent",
     "Group"
