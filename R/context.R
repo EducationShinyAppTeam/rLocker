c0bca6456cfb3671022eb985ae589eb2a68f81b2
ROBERT PATRICK CAREY III <rpc5102@psu.edu>
2019-07-30 15:43:36 -0400

    added context skeleton

---
+++ R/context.R
@@ -0,0 +1,9 @@
+#'context
+#'
+#' xAPI Context object definitions
+#' 
+#' @name context
+#' @section Details:
+#'   An optional property that provides a place to add contextual information to a Statement.
+#' @seealso \link{https://github.com/adlnet/xAPI-Spec/blob/master/xAPI-Data.md#context}
+NULL
